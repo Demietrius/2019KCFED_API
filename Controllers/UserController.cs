5862ddf2b9c8e5e6465c58f1754b634c116c701f
petercopus <petercopus@gmail.com>
2019-10-26 13:21:01 -0500

    Added UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using _2019KCFED_API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _2019KCFED_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DbConnection _context;
+        User testUser = new User();
+        
+        public UserController(DbConnection context)
+        {
+            _context = context;
+
+            testUser.UserId = 1;
+            testUser.UserName = "Test";
+
+            PostUser(testUser);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            return user;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new {id = user.UserId}, user);
+        }
+
+
+    }
+}
